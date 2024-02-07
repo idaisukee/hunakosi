036e1e1bebff10412727298e58bcabcd06f1842a
ieremius <hibariyashiki@gmail.com>
2024-02-07 11:26:25 +0900

    hunakosi-cd: use zle.

--- plugins/hunakosi/hunakosi.plugin.zsh
+++ plugins/hunakosi/hunakosi.plugin.zsh
@@ -1,5 +1,12 @@
 hunakosi-cd() {
-    builtin cd $(find . -type d -maxdepth 1 | fzf-tmux -p --reverse )
+    local _dir="$(find . -type d -maxdepth 1 | fzf-tmux -p --reverse )"
+    if [[ -n ${_dir} ]]; then
+        local _cmd="builtin pushd ${_dir}"
+        BUFFER=$_cmd
+        zle accept-line
+        unset _cmd
+    fi
+    unset _dir
 }
 zle     -N            hunakosi-cd
 bindkey -M vicmd '^H' hunakosi-cd
