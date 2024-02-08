685d2191283eb7d3628a5f77d5d743bc67d0a2cb
ieremius <hibariyashiki@gmail.com>
2024-02-08 12:39:31 +0900

    del hunakosi-cd.

--- plugins/hunakosi/hunakosi.plugin.zsh
+++ plugins/hunakosi/hunakosi.plugin.zsh
@@ -1,13 +1,5 @@
-hunakosi-cd() {
-    local _dir="$(find . -type d -maxdepth 1 | fzf-tmux -p --reverse )"
-    if [[ -n ${_dir} ]]; then
-        local _cmd="builtin pushd ${_dir}"
-        BUFFER=$_cmd
-        zle accept-line
-        unset _cmd
-    fi
-    unset _dir
-}
-zle     -N            hunakosi-cd
-bindkey -M vicmd '^H' hunakosi-cd
-bindkey -M viins '^H' hunakosi-cd 
+zle     -N            fzf-cd-widget
+bindkey -M vicmd '^H' fzf-cd-widget
+bindkey -M viins '^H' fzf-cd-widget
+
+FZF_ALT_C_COMMAND='find . -type d -maxdepth 1'
